7128965fc762d8ebd4200d30b7a80c3f73776a7a
Eneko Lakasta <enekolakast@gmail.com>
2015-11-18 19:27:48 +0100

    add mongodb

--- api-server/server.js
+++ api-server/server.js
@@ -1,34 +1,37 @@
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express    = require('express');        
+var app        = express();                 
 var bodyParser = require('body-parser');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.API_PORT || 3000;        // set our port
+var port = process.env.API_PORT || 3000;
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+//connect to database
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://' + process.env.MONGODB_USERNAME + ':' + process.env.MONGODB_PASSWORD + '@mongodb:' + process.env.MONGODB_PORT + '/restdb',
+	function(err) {
+    console.log(err);
 });
 
-// more routes for our API will happen here
+var Note     = require('./app/models/note');
+
+
+
+// ROUTES
+// =============================================================================
+var router = express.Router();       
+
+// test route http://domain:port/api
+router.get('/', function(req, res) {
+    res.json({ message: 'Api server up&running' });   
+});
+
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
 
-// START THE SERVER
-// =============================================================================
+
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('Api Server started on port ' + port);
